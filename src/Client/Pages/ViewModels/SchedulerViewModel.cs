e8980ba4071f51cd512020cbf38113394f21b5c1
£ukasz Szafirski <lukasz.szafirski@ac-project.net>
2019-12-09 21:31:33 +0100

    schedulers

---
+++ src/Client/Pages/ViewModels/SchedulerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GasWeb.Shared.Schedulers;
+
+namespace GasWeb.Client.Pages.ViewModels
+{
+    public class SchedulerViewModel
+    {
+        public SchedulerViewModel(Scheduler model)
+        {
+            Model = model;
+        }
+
+        public Scheduler Model { get; }
+
+        public string Interval
+        {
+            get => Model.Interval.ToString();
+            set { }
+        }
+
+        public bool Started
+        {
+            get => Model.StartedAt != null;
+            set { }
+        }
+    }
+}
